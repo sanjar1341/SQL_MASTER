133ff061d5f035959858f3594ffb9228b624e679
Sanjarbek <normurodovsanjar100@gmail.com>
2024-12-18 22:39:07 +0500

    main.py fayli yaratildi va kerakli kutubxonalar import qilindi

---
+++ main.py
@@ -0,0 +1,4 @@
+from PyQt5.QtWidgets import QApplication, QPlainTextEdit, QWidget, QVBoxLayout, QMainWindow, QTextEdit
+from PyQt5.QtGui import QColor, QPainter, QTextFormat
+from PyQt5.QtCore import Qt, QRect, QSize
+import sys
